083908da345516a9bb1ea10aaf8ce7a7b2747294
sallyselva <166712783+sallyselva@users.noreply.github.com>
2025-01-18 12:49:20 +0530

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 })
 let Pizza: Sprite = null
 scene.setBackgroundColor(7)
-let mySprite = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 Pizza = sprites.create(img`
     . . . . . . b b b b . . . . . .
