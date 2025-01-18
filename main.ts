4e6585355f3bf4945a9f17f62040927181d3f09b
sallyselva <166712783+sallyselva@users.noreply.github.com>
2025-01-18 12:02:09 +0530

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -15,23 +15,23 @@ scene.setBackgroundColor(7)
 let mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 Pizza = sprites.create(img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
-    `, SpriteKind.Food)
+    . . . . . . b b b b . . . . . .
+    . . . . . . b 4 4 4 b . . . . .
+    . . . . . . b b 4 4 4 b . . . .
+    . . . . . b 4 b b b 4 4 b . . .
+    . . . . b d 5 5 5 4 b 4 4 b . .
+    . . . . b 3 2 3 5 5 4 e 4 4 b .
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e
+    . . . b 5 3 2 3 5 5 5 5 e e e e
+    . . b d 7 5 5 5 3 2 3 5 5 e e e
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 .
+    b d 3 2 d 5 5 5 d d d 4 4 . . .
+    b 5 5 5 5 d d 4 4 4 4 . . . . .
+    4 d d d 4 4 4 . . . . . . . . .
+    4 4 4 4 . . . . . . . . . . . .
+`, SpriteKind.Food)
 info.startCountdown(30)
 mySprite.setStayInScreen(true)
 Pizza.setStayInScreen(true)
